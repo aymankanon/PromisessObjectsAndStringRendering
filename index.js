0fda260695373c47812669724c1712efc2cba722
aymankanon <aymanaboukanon@gmail.com>
2017-10-20 12:12:17 +0200

    the homework is running

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 // The goal is render a github informations on a page with:
 // Promises, Map and Reduce.
 
-function GitHubUser (username) {
-    // Fill this
+function httpGetAsync(theUrl, callback1) {
+     let xmlHttp = new XMLHttpRequest();
+     xmlHttp.onreadystatechange = function () {
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+               callback1(JSON.parse(xmlHttp.responseText));
+              }
+     xmlHttp.open("GET", theUrl, true);
+     xmlHttp.send();
+
 }
 
-GitHubUser.prototype.getUserInformation = function () {
-    // Fill this
-};
+class GitHubUser {
+     constructor(username) {
+          this.username = username;
 
-GitHubUser.prototype.getRepos = function () {
-    // Fill this
+     }
+     getUserInformation() {
+          let theUrl = 'https://api.github.com/users/' + this.username;
+          return new Promise(function (resolve) {
+               httpGetAsync(theUrl, resolve);
+          });
+     }
+     getRepos() {
+          let theUrl = 'https://api.github.com/users/' + this.username + '/repos';
+          return new Promise(function (resolve) {
+               httpGetAsync(theUrl, resolve);
+          });
+
+     }
+     render() {
+          let list = "<h1>User information</h1>";
+          console.log(this.user);
+          list += '<h3>' + 'Id: ' + this.user.login + '<br>' + 'Name: ' + this.user.name + '</h3>' + '<img width=250px height=250px src="' + this.user.avatar_url + '">';
+          list += '<h2>Repository informations</h2>' + `<ul>
+         ${this.repos.reduce((p, i) => p + `<li><a target="_blank" href="${i.html_url}">${i.name}</a></li>`, '')}
+         </ul>`;
+          return list;
+     }
 }
 
-GitHubUser.prototype.render = function () {
-    // Here return a string, be fancy and use map && reduce
-}
-
-function Render($element, html){
-    // Fill this
+function Render(element, html) {
+     document.getElementById(element).innerHTML = html;
 }
 
 
-// Expecting
-let gitUser = new GitHubUser('ideabile');
+let gitUser = new GitHubUser('aymankanon');
 
-gitUser
-    .getUserInformations()
-    .then(function(informations){
-        this.user = informations;
-        return this.getRepos();
-    })
-    .then(function(repos) {
-        this.repos = repos;
-        Render('.githubView', this.render());
-    });
+gitUser.getUserInformation().then(function (information) {
+     gitUser.user = information;
+     return gitUser.getRepos();
+}).then(function (repos) {
+     gitUser.repos = repos;
+     Render('githubView', gitUser.render());
+});
